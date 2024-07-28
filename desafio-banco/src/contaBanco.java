6b2a56a3efb756d1ada051e9c88179ddcde12874
robson <robson.silva@dcx.ufpb.br>
2024-07-28 03:19:32 -0300

    projeto finalizado

--- desafio-banco/src/contaBanco.java
+++ desafio-banco/src/contaBanco.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class contaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Nome do cliente: ");
+        String nomeCliente = leitor.nextLine();
+
+        System.out.println("Digite os quatro números da AGÊNCIA no formato '012-3': ");
+        String agencia = leitor.nextLine();
+
+        System.out.println("Digite os quatro números da CONTA: ");
+        int conta = Integer.parseInt(leitor.nextLine());
+
+        double saldo = 0.0; //*saldo inicial
+        
+        System.out.printf("Olá, %s!"+" Obrigado por criar uma conta em nosso banco. Sua Agência é %s"+", conta %d"+" e seu saldo é R$ %.2f",nomeCliente,agencia,conta,saldo);
+
+        leitor.close();
     }
 }
---
+++ desafio-banco/src/teste.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class teste {
+    public static void main(String[] args) {
+        
+        Random gerador = new Random();
+        System.out.println(gerador.nextInt(5));
+    }
+}
