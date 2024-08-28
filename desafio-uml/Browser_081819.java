10094fa948c770467414955a9432ad67227cd6fb
Robson <126630455+robson-morais@users.noreply.github.com>
2024-08-28 11:46:23 -0300

    Update Browser_081819.java

--- desafio-uml/Browser_081819.java
+++ desafio-uml/Browser_081819.java
@@ -2,4 +2,12 @@ package uml;
 
 public class Browser {
 
+  public static void main(String[]args){
+    System.out.println("Pesquisar: ");
+    if (validateConection){
+      System.out.println("Mostrando resultados da pesquisa...");
+    } else {
+      System.out.println("Voçê não está conectado a Internet");
+    }
+  }
 }
